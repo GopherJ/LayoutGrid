9c175fe49342c2144629cb3729792fb2e2d31375
Cheng JIANG <cheng.jiang@outlook.fr>
2018-07-21 19:15:33 +0200

    [FEATURE]: Download json or csv data

--- src/components/LayoutGrid/Table.vue
+++ src/components/LayoutGrid/Table.vue
@@ -8,11 +8,34 @@
         :focusable="focusable"
         paginated
         :per-page="perPage">
+
         <template slot-scope="props">
             <b-table-column v-for="(value, key) in props.row" :field="key" :label="key" :key="key" centered>
                 {{ value }}
             </b-table-column>
         </template>
+
+        <div slot="bottom-left">
+            <strong>Export:  </strong>
+
+            <a :href="json" download="data.json">
+                <span class="icon">
+                    <i class="mdi mdi-download mdi-18px"></i>
+                </span>
+                <span>
+                    json
+                </span>
+            </a>
+
+            <a :href="csv" download="data.csv">
+                <span class="icon">
+                    <i class="mdi mdi-download mdi-18px"></i>
+                </span>
+                <span>
+                    csv
+                </span>
+            </a>
+        </div>
     </b-table>
 </template>
 
@@ -45,6 +68,27 @@
             perPage: {
                 type: Number,
                 default: 5
+            }
+        },
+        computed: {
+            csv() {
+                if (this.data.length > 0) {
+                    const [first] = this.data,
+                        keys = Object.keys(first);
+
+                    const replacer = (key, value) => value === null ? '' : value;
+                    const data = this.data.reduce((ite, cur) => {
+                        ite += '\r\n';
+                        ite += keys.map(key => JSON.stringify(cur[key], replacer)).join(',');
+
+                        return ite;
+                    }, keys.join(','));
+
+                    return `data:text/csv;charset=utf-8,${data}`;
+                }
+            },
+            json() {
+                return `data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(this.data))}`;
             }
         }
     }
